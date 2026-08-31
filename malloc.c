5200f0eebf88ea232cfbe760662f3af0fc5e4a4f
Renan <renandev.backend@gmail.com>
2026-08-31 03:04:52 -0300

    malloc no learning (~)

---
+++ malloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int number = 0;
+    printf("enter the number of grades: ");
+    scanf("%d", &number);
+
+    char *grades = malloc(number * sizeof(char));
+
+    if(grades == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    for(int i = 0; i < number; i++) {
+        printf("enter grade number #%d", i + 1);
+        scanf(" %c", &grades[i]);
+    }
+
+    for(int i = 0; i < number; i++) {
+        printf("%c", grades[i]);
+    }
+    
+    free(grades);
+    grades = NULL; 
+
+    return 0;
+}
